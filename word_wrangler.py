e0f6506b1a1e539b3bde8d8a92438ab660d85827
Jordan Weiner <weinerj8@gmail.com>
2020-09-04 17:22:56 -0700

    Cleaned up comments

--- word_wrangler.py
+++ word_wrangler.py
@@ -1,5 +1,6 @@
 import random
 
+# Generates a randomly scrambled version of the input word
 def scramble_word(word):
   chars = list(word)
   random.shuffle(chars)
@@ -7,20 +8,22 @@ def scramble_word(word):
   scrambled = scrambled.join(chars)
   return scrambled
 
+# Will display the appropriate number of available words to find, plus any correctly guessed hidden words so far
+# Called each time for words of varying lengths (so called once for hidden words of length 3, then again for hidden words of length 4, etc.)
 def display_words(word_length, answers):
-  # figure out which guesses are the length we're looking for
+  # First, figure out which correct guesses are the appropriate length to display
   guesses_to_display = []
   for guess in correct_guesses:
     if len(guess) == word_length:
       guesses_to_display.append(guess)
 
-  # figure out how many answers are of that length
+  # Next, figure out how many answers are the appropriate length
   answer_amount = 0
   for answer in answers:
     if len(answer) == word_length:
       answer_amount += 1
 
-  # print nums in ascending order, indicating how many answers there are of that length, and correct guesses if applicable
+  # Print numbers in ascending order, indicating how many answers there are of that length, and correct guesses if applicable
   n = 0
   while n < answer_amount:
     if n < len(guesses_to_display):
@@ -29,6 +32,7 @@ def display_words(word_length, answers):
       print(f"  {n+1})")
     n+=1
 
+# Displays player options
 def display_menu():
   print("\n--------------------\n")
   print("Guess a word or input one of the following:")
@@ -37,6 +41,7 @@ def display_menu():
   print("  [Q]UIT")
   print("\n--------------------\n")
 
+# Displays current master word and player points, and calls the display_words function as many times as necessary, beginning with hidden words of length 3
 def display_progress(master_word, answers, current_points):
   print("\n--------------------\n")
   print(f"Your scrambled master word is: '{master_word}'")
@@ -49,9 +54,9 @@ def display_progress(master_word, answers, current_points):
     length += 1
 
 
-# reveal word process
-    # goal - append a random word from answers to the correct_guesses list
-    # generate randint for to get random index from answers list
+# Appends a random word from the answers list to the correct_guesses list
+# Does this by generating a randint to use as a index from answers list
+# By adding to correct_guesses, this means it will now be displayed in display_words
 def reveal_random_word(answers):
   while True:
     index = random.randint(0, len(answers) - 1)
@@ -61,12 +66,16 @@ def reveal_random_word(answers):
       break
   return random_word
 
+# Adds up possible points based on lengths of each word in answers for the particular level
 def calculate_possible_points(answers):
     possible_points = 0
     for answer in answers:
         possible_points += len(answer)
     return possible_points
 
+# Runs each level, asking player for input and responding accordingly
+# Originally had a separate function for player input, but ran into trouble with scope and variables, particularly for player points, and figuring out how to break the loop if the player quits or beats the level.
+# Each time play_level is called, it also updates the player's point total, so their points will be tracked level to level.
 def play_level(master_word, answers, player_points):
   current_points = player_points
 
@@ -98,13 +107,13 @@ def play_level(master_word, answers, player_points):
       print("\n~~~~~~~~~~~~~~~~~~~~~~~\n")
       print("LEVEL COMPLETE!")
       print(f"\nYour score: {current_points} out of {total_possible_points} possible points")
-      print("\n~~~~~~~~~~~~~~~~~~~~~~~\n")
+      print("\n~~~~~~~~~~~~~~~~~~~~~~~")
       break
 
   return current_points
 
 
-## GAME INTRO MOCKUP ##
+############# GAME INTRO ############
 
 print("\nWelcome to WORD WRANGLER!")
 print("\nIn each round, you'll receive a scrambled MASTER WORD.")
@@ -122,7 +131,9 @@ while True:
     break
 
 
-### LEVEL ONE TEST - "house" ###
+
+
+############ LEVEL ONE - "house" #############
 
 level1_master_word = scramble_word("house")
 level1_answers = ["hoe", "sue", "hue", "she", "use", "shoe", "hoes", "hues", "hose", "house"]
@@ -131,9 +142,13 @@ total_possible_points += calculate_possible_points(level1_answers)
 
 correct_guesses = []
 
+print("\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> LEVEL 1 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
 player_points = play_level(level1_master_word, level1_answers, player_points)
 
-### LEVEL TWO TEST - "noodle" ###
+
+
+
+############### LEVEL TWO - "noodle" #############
 
 level2_master_word = scramble_word("noodle")
 level2_answers = ["nod", "end", "loo", "one", "ode", "led", "eon", "den", "don", "old", "doe", "ole", "dole", "lend", "lone", "noel", "done", "lode", "node", "loon", "olden", "noodle"]
@@ -142,4 +157,5 @@ total_possible_points += calculate_possible_points(level2_answers)
 
 correct_guesses = []
 
+print("\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> LEVEL 2 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
 player_points = play_level(level2_master_word, level2_answers, player_points)
