1311d21d64e6f6aecdb74240ed4f993758dfbe63
Jordan Weiner <weinerj8@gmail.com>
2020-09-10 11:31:35 -0700

    Added more sets of words/answers

--- word_wrangler.py
+++ word_wrangler.py
@@ -7,8 +7,10 @@ points = {
 
 words_and_answers = [
   {"word": "house", "answers": ["hoe", "sue", "hue", "she", "use", "shoe", "hoes", "hues", "hose", "house"]},
+    {"word" : "bright", "answers": ["bright", "birth", "girth", "right", "brig", "grit", "big", "bit", "hit", "rib", "rig"]},
   {"word": "noodle", "answers": ["nod", "end", "loo", "one", "ode", "led", "eon", "den", "don", "old", "doe", "ole", "dole", "lend", "lone", "noel", "done", "lode", "node", "loon", "olden", "noodle"]},
-  {"word": "forest", "answers": ["forest", "fortes", "foster", "softer", "fores", "forte", "forts", "frets", "frost", "store", "foes", "fore", "fort", "fret", "ores", "rest", "rose", "rote", "rots", "soft", "sore", "sort", "toes", "tore", "foe", "for", "fro", "ore", "rot", "set", "toe"]}
+  {"word": "forest", "answers": ["forest", "fortes", "foster", "softer", "fores", "forte", "forts", "frets", "frost", "store", "foes", "fore", "fort", "fret", "ores", "rest", "rose", "rote", "rots", "soft", "sore", "sort", "toes", "tore", "foe", "for", "fro", "ore", "rot", "set", "toe"]},
+  {"word": "project", "answers": ["project", "copter", "crept", "trope", "cope", "core", "crop", "pert", "poet", "pore", "port", "rope", "rote", "tore", "cot", "jet", "jot", "opt", "ore", "per", "pet", "pot", "rot", "toe", "top"]}
 ]
 
 # Generates a randomly scrambled version of the input word
